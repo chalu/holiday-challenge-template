f7abdbc49f504837db0d8d482587c203a20a39e8
Charles Opute Odili <chaluwa@gmail.com>
2022-07-15 15:55:57 +0000

    init template

---
+++ src/js/app.mjs
@@ -0,0 +1,8 @@
+function startApp() {
+    // make magic in here!
+
+    // Your entire app should not necessarily be coded inside this single function (though there's no penalty for that), 
+    // so create and use/call additional functions from here
+};
+
+export default startApp;
